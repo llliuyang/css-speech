6290baacea6d12dffc2f44693cd283f600197827
llliuyang <1714610279@qq.com>
2024-01-07 18:23:05 +0800

    引入revealjs依赖

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './style.css'
 import App from './App.vue'
 import Router from "./router/index"
 
-
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+import 'reveal.js/plugin/highlight/monokai.css'
 
 const app = createApp(App)
 app.use(Router)
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,3 +3,7 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'reveal.js'
+declare module 'reveal.js/plugin/markdown/markdown.js'
+declare module 'reveal.js/plugin/highlight/highlight.js'
